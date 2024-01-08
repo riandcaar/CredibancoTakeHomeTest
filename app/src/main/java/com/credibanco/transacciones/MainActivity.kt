3fc4b40d09ad872f706290fe80a0ddb39610fc4c
Ricardo Casallas <ricardoandrescasallas@gmail.com>
2024-01-08 09:47:51 -0500

    Main Activity ok

--- app/src/main/java/com/credibanco/transacciones/MainActivity.kt
+++ app/src/main/java/com/credibanco/transacciones/MainActivity.kt
@@ -7,22 +7,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.credibanco.transacciones.ui.navigation.AppNavGraph
 import com.credibanco.transacciones.ui.screens.auth.TransactionAuthScreen
 import com.credibanco.transacciones.ui.theme.TakeHomeTestTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContent {
             TakeHomeTestTheme {
-                // A surface container using the 'background' color from the theme
+                navController = rememberNavController()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TransactionAuthScreen()
+                    AppNavGraph(navController = navController)
                 }
             }
         }
