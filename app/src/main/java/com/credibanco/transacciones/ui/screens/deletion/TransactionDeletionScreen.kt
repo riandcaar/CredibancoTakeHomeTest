20ebc597e7d9ce0318c14bb4f0e57a5929029404
Ricardo Casallas <ricardoandrescasallas@gmail.com>
2024-01-08 15:28:58 -0500

    UI Ok

--- app/src/main/java/com/credibanco/transacciones/ui/screens/deletion/TransactionDeletionScreen.kt
+++ app/src/main/java/com/credibanco/transacciones/ui/screens/deletion/TransactionDeletionScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
