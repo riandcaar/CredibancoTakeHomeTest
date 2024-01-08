c81c3e6e0d7f49731eed3d1592094dbcae85ae1f
Ricardo Casallas <ricardoandrescasallas@gmail.com>
2024-01-08 09:34:20 -0500

    Nav Implement ok

---
+++ app/src/main/java/com/credibanco/transacciones/ui/navigation/AppNavGraph.kt
@@ -0,0 +1,51 @@
+package com.credibanco.transacciones.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.credibanco.transacciones.ui.screens.auth.TransactionAuthScreen
+import com.credibanco.transacciones.ui.screens.auth.TransactionAuthViewModel
+import com.credibanco.transacciones.ui.screens.deletion.TransactionDeletion
+import com.credibanco.transacciones.ui.screens.list.TransactionListScreen
+import com.credibanco.transacciones.ui.screens.search.TransactionSearchScreen
+
+@Composable
+fun AppNavGraph(navController: NavHostController) {
+
+
+    val authViewModel: TransactionAuthViewModel = hiltViewModel()
+    val deletionViewModel: TransactionDeletionViewModel = hiltViewModel()
+    val listViewModel: TransactionListViewModel = hiltViewModel()
+    val searchViewModel: TransactionSearchViewModel = hiltViewModel()
+
+    NavHost(navController = navController, startDestination = Routes.TransactionAuth) {
+
+        composable(Routes.TransactionAuth) {
+            TransactionAuthScreen(
+                transactionAuthViewModel = authViewModel,
+                navController = navController
+            )
+        }
+
+        composable(Routes.TransactionDeletion) {
+            TransactionDeletion(transactionDeletionViewModel = deletionViewModel, navController = navController)
+        }
+        composable(Routes.TransactionList) {
+            TransactionListScreen(transactionListViewModel = listViewModel,navController = navController)
+        }
+
+        composable(Routes.SearchTransaction) {
+            TransactionSearchScreen(transactionSearchViewModel = searchViewModel,navController = navController)
+        }
+        composable("${Routes.TransactionInfo}/{receipt}") { backStackEntry ->
+            val receipt = backStackEntry.arguments?.getString("receipt") ?: return@composable
+            TransactionInfoScreen(
+                transactionInfoViewModel = hiltViewModel(),
+                navController = navController,
+                receipt = receipt
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/credibanco/transacciones/ui/navigation/Routes.kt
@@ -0,0 +1,9 @@
+package com.credibanco.transacciones.ui.navigation
+
+object Routes {
+    const val TransactionAuth = "transactionAuth"
+    const val TransactionDeletion = "transactionDeletion"
+    const val TransactionList = "transactionList"
+    const val TransactionInfo = "transactionInfo"
+    const val SearchTransaction = "searchTransaction"
+}
