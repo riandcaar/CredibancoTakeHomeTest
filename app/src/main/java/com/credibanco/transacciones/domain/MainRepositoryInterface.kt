7054e16d8636652ac0e7f02b784ec15c84a90005
Ricardo Casallas <ricardoandrescasallas@gmail.com>
2024-01-08 09:30:36 -0500

    Domain and Interface contract ok

--- app/src/main/java/com/credibanco/transacciones/domain/MainRepositoryInterface.kt
+++ app/src/main/java/com/credibanco/transacciones/domain/MainRepositoryInterface.kt
@@ -1,8 +1,11 @@
 package com.credibanco.transacciones.domain
 
+import com.credibanco.transacciones.data.network.dtos.responses.TransactionAnnulationResponse
 import com.credibanco.transacciones.ui.models.Transaction
 import com.credibanco.transacciones.ui.models.TransactionResult
 
 interface MainRepositoryInterface {
    suspend fun authorizeTransaction(transaction: Transaction) :Result<TransactionResult>
+
+   suspend fun  annulTransaction(transactionId: String, rrn: String): Result<TransactionAnnulationResponse>
 }
